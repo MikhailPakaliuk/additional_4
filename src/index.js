21135e987bb14138d726a508d63de97bd365826b
Michail Dyurilen <mr.dyurilen@mail.ru>
2017-09-26 19:05:03 +0300

    error correction test #3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function multiply(first, second) {
   let notation =10;
-  return (parseInt(first,notation)*parseInt(second,notation)).toString;
+  return (parseInt(first,notation)*parseInt(second,notation)).toString();
 }
